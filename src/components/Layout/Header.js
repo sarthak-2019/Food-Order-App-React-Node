48e475cbaf62acb40ede9302eae7825ef74dd099
Sarthak Shukla <sarthak2019fractal@gmail.com>
2021-07-17 00:10:53 +0530

    Added Meals and Header Components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import mealsImage from "./../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of foods" />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;
